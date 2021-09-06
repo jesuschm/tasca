8d16714764e91c12bae24d634ed95be6e221f777
jesuschm <jesuschaconmontero@gmail.com>
2021-09-06 13:06:32 +0200

    Commands management included.

--- src/tasca.py
+++ src/tasca.py
@@ -11,38 +11,48 @@ def main():
         if _repo.client:
             print("[+] Hello friend! Welcome to the Tasca. Get fun! ")
             print("[+] Control + C to exit.\n")
+            
             while command != "^C":
-                command = str(input("> "))
-                
-                # Posting command
-                if '->' in command:
-                    data = command.split(" -> ")    
-                    if len(data) == 2:
-                        post(_repo, user = data[0], message = data[1])
+                try:
+                    command = str(input("> "))
+                    
+                    # Posting command
+                    if '->' in command:
+                        data = command.split(" -> ")    
+                        if len(data) == 2:
+                            post(_repo, username = data[0], message = data[1])
+                        else:
+                            print("[-] Bad post command. Correct format: [username] -> [message].")
+                            
+                    elif 'follows' in command:
+                        data = command.split(" follows ")
+                        if len(data) == 2:
+                            user = data[0]
+                            follow_user = data[1]
+                            
+                            rc = follow(_repo, username = user, follow_username = follow_user)
+                            if rc:
+                                print(f"[+] {user} is now following {follow_user}.")
+                            else:
+                                print(f"[-] Error trying to follow {follow_user}")
+                        else:
+                            print("[-] Bad follow command. Correct format: [username] -> [username].")
+                            
+                    elif 'wall' in command:
+                        data = command.split(" wall")
+                        if len(data) == 2 and data[1] == '':
+                            wall(_repo, username = data[0])
+                        else:
+                            print("[-] Bad wall command. Correct format: [username] wall.")
+                            
                     else:
-                        print("[-] Bad post command. Correct format: [username] -> [message].")
-                        
-                elif 'follows' in command:
-                    data = command.split(" follows ")
-                    if len(data) == 2:
-                        follow(user = data[0], follow = data[1])
-                    else:
-                        print("[-] Bad follow command. Correct format: [username] -> [username].")
-                        
-                elif 'wall' in command:
-                    data = command.split(" wall")
-                    if len(data) == 1:
-                        wall(user = data[0])
-                    else:
-                        print("[-] Bad wall command. Correct format: [username] wall.")
-                        
-                else:
-                    data = command.split(" ")
-                    if len(data) == 1:
-                        read(user = command)
-                    else:
-                        print("[-] Bad username to read. Usernames don't contain spaces.")
-                        
+                        data = command.split(" ")
+                        if len(data) == 1:
+                            read(_repo, username = command)
+                        else:
+                            print("[-] Bad username to read. Usernames don't contain spaces.")
+                except Exception as e:
+                    print(f"[-] Error: {e}.")
         else:
             raise("Database not connected.")
     except KeyboardInterrupt:
